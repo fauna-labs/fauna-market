3e749efd0f456a90fea8c19c07c40b16f8551579
Chris Anderson <chris@fauna.com>
2019-05-20 09:17:59 -0700

    add comments to model

--- src/model.js
+++ src/model.js
@@ -3,6 +3,7 @@ import faunadb, {query as q} from 'faunadb';
 
 export default class Model {
   constructor () {
+    // set the default data for an empty page
     this.message = "";
     this.onChanges = [];
     this.items = [];
@@ -10,13 +11,86 @@ export default class Model {
     this.purchases = [];
     this.client = null;
   }
+  // The application subscribes here and the model calls it's render callback
+  // when the underlying data changes.
   subscribe(onChange) {
     this.onChanges.push(onChange);
   }
+  // This function is called when the model has changed data and should
+  // tell the application to repaint.
   inform() {
     console.log("inform", this);
     this.onChanges.forEach((cb) => cb());
   }
+  // The core transactional logic. See docs.fauna.com for a detailed walkthrough
+  transactItem(item, player) {
+    return this.client.query(
+      q.Let({
+        // Load the current player and item based on their refs.
+        buyer : q.Get(player.ref),
+        item : q.Get(item.ref)
+      }, q.Let({
+        // Load the seller account and set the transaction price.
+        isForSale : q.Select(["data", "for_sale"], q.Var("item")),
+        itemPrice : q.Select(["data", "price"], q.Var("item")),
+        buyerBalance : q.Select(["data", "credits"], q.Var("buyer")),
+        seller : q.Get(q.Select(["data", "owner"], q.Var("item")))
+      },
+        // Check that the item is for sale.
+        q.If(q.Not(q.Var("isForSale")),
+          "purchase failed: item not for sale",
+          q.If(q.Equals(q.Select("ref", q.Var("buyer")), q.Select("ref", q.Var("seller"))),
+            // Attempting to buy an item you are selling, removes it from sale
+            q.Do(
+              q.Update(q.Select("ref", q.Var("item")), {
+                data : {
+                  for_sale : false
+                }
+              }),
+              "item removed from sale"
+            ),
+            // Check the credit balance of the purchasing player to ensure
+            // they have available funds.
+            q.If(q.LT(q.Var("buyerBalance"), q.Var("itemPrice")),
+              "purchase failed: insufficient funds",
+              // All clear! Preconditions passed.
+              q.Do(
+                // Write a purchase record.
+                q.Create(q.Class("purchases"), {
+                  data : {
+                    item : q.Select("ref", q.Var("item")),
+                    price : q.Var("itemPrice"),
+                    buyer : q.Select("ref", q.Var("buyer")),
+                    seller : q.Select("ref", q.Var("seller"))
+                  }
+                }),
+                // Update the balances of the selling player and the purchasing player.
+                q.Update(q.Select("ref", q.Var("buyer")), {
+                  data : {
+                    credits : q.Subtract(q.Var("buyerBalance"), q.Var("itemPrice"))
+                  }
+                }),
+                q.Update(q.Select("ref", q.Var("seller")), {
+                  data : {
+                    credits : q.Add(q.Select(["data", "credits"], q.Var("seller")), q.Var("itemPrice"))
+                  }
+                }),
+                // Update the item with the new owner.
+                q.Update(q.Select("ref", q.Var("item")), {
+                  data : {
+                    owner : q.Select("ref", q.Var("buyer")),
+                    for_sale : false
+                  }
+                }),
+                "purchase success"
+              )
+            )
+          )
+         )))
+    );
+  }
+
+  // Lifecycle hook that allows
   refresh(message) {
     Promise.all([
       this.queryItemsForSale().then(({data: items}) => this.items = items),
@@ -31,19 +105,22 @@ export default class Model {
       this.inform();
     });
   }
+
   setSellingState(item, player) {
     console.log("setSellingState", this.items.indexOf(item, this.items), item, this.items)
     this.items.splice(this.items.indexOf(item, this.items), 1);
   }
+
   sell(item, player) {
     // todo set preliminary "waiting" state of success and be ready to remove it on error.
     this.setSellingState(item, player);
     this.inform();
-    return this.sellItemToPlayer(item, player).then((r) => {
+    return this.transactItem(item, player).then((r) => {
       this.refresh(r);
       return r;
     });
   }
+
   makeForSale(item, stringPrice, isForSale) {
     const price = parseInt(stringPrice, 10)
     if (isNaN(price) || price < 0)  {
@@ -55,6 +132,7 @@ export default class Model {
       return r;
     });
   }
+
   setupSession() {
     var secret = window.location.hash;
     if (secret) {
@@ -120,75 +198,9 @@ export default class Model {
     }))
   }
 
-
-
-  sellItemToPlayer(item, player) {
-    return this.client.query(
-      q.Let({
-        buyer : q.Get(player.ref),
-        item : q.Get(item.ref)
-      }, q.Let({
-        isForSale : q.Select(["data", "for_sale"], q.Var("item")),
-        itemPrice : q.Select(["data", "price"], q.Var("item")),
-        buyerBalance : q.Select(["data", "credits"], q.Var("buyer")),
-        seller : q.Get(q.Select(["data", "owner"], q.Var("item")))
-      }, q.If(q.Not(q.Var("isForSale")),
-          "purchase failed: item not for sale",
-          q.If(q.Equals(q.Select("ref", q.Var("buyer")), q.Select("ref", q.Var("seller"))),
-            q.Do(
-              q.Update(q.Select("ref", q.Var("item")), {
-                data : {
-                  for_sale : false
-                }
-              }),
-              "item removed from sale"
-            ),
-            // check balance
-            q.If(q.LT(q.Var("buyerBalance"), q.Var("itemPrice")),
-              "purchase failed: insufficient funds",
-
-              // all clear! record the purchase, update the buyer, seller and item.
-              q.Do(
-                q.Create(q.Class("purchases"), {
-                  data : {
-                    item : q.Select("ref", q.Var("item")),
-                    price : q.Var("itemPrice"),
-                    buyer : q.Select("ref", q.Var("buyer")),
-                    seller : q.Select("ref", q.Var("seller"))
-                  }
-                }),
-                q.Update(q.Select("ref", q.Var("buyer")), {
-                  data : {
-                    credits : q.Subtract(q.Var("buyerBalance"), q.Var("itemPrice"))
-                  }
-                }),
-                q.Update(q.Select("ref", q.Var("seller")), {
-                  data : {
-                    credits : q.Add(q.Select(["data", "credits"], q.Var("seller")), q.Var("itemPrice"))
-                  }
-                }),
-                q.Update(q.Select("ref", q.Var("item")), {
-                  data : {
-                    owner : q.Select("ref", q.Var("buyer")),
-                    for_sale : false
-                  }
-                }),
-                "purchase success"
-              )
-            )
-          )
-         )))
-    );
-  }
-
   queryPlayerItems(players) {
     const refs = players.map((p) => p.ref);
     console.log("queryPlayerItems", refs);
-
-    // return client.query(
-    //   q.Paginate(q.Match(q.Index("items_by_owner"), players[0].ref))
-    // )
-
     return this.client.query(
       q.Map(refs, (ref) => q.Select("data", q.Map(
         q.Paginate(q.Match(q.Index("items_by_owner"), ref)),
